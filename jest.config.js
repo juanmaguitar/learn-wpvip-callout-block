3cafc732c2bc32a4b125d6c916f0f18ec12444ea
JuanMa <juanma.garrido@gmail.com>
2025-04-05 18:26:38 +0100

    Refactor Jest configuration and remove obsolete setup and mock files
    - Streamlined jest.config.js by removing unnecessary customizations and mock mappings.
    - Deleted jest.setup.js and styleMock.js as they were no longer needed, simplifying the test setup.

--- jest.config.js
+++ jest.config.js
@@ -2,26 +2,10 @@ const defaultConfig = require("@wordpress/scripts/config/jest-unit.config");
 
 module.exports = {
 	...defaultConfig,
-	// Add your customizations here
-	setupFilesAfterEnv: [
-		...(defaultConfig.setupFilesAfterEnv || []),
-		"<rootDir>/jest.setup.js",
-	],
 	// Example: Add custom module name mappings
 	moduleNameMapper: {
 		...defaultConfig.moduleNameMapper,
-		"^@components/(.*)$": "<rootDir>/test/__mocks__/components/$1",
-		// Mock file and style imports.
 		"\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
 			"<rootDir>/test/__mocks__/fileMock.js",
-		"\\.(css|scss)$": "<rootDir>/test/__mocks__/styleMock.js",
-		// Mock all wordpress dependencies that actually externals.
-		//"\\@wordpress/(.+)$": "<rootDir>/test/__mocks__/@wordpress/$1.js",
 	},
-	// Add test directory pattern
-	testMatch: [
-		"<rootDir>/test/**/*.test.js",
-		"<rootDir>/src/**/test/**/*.test.js",
-		"<rootDir>/src/**/__tests__/**/*.test.js",
-	],
 };
--- jest.setup.js
+++
@@ -1,7 +0,0 @@
-const React = require( 'react' );
-
-global.wp = global.wp || {};
-
-global.wp.element = global.wp.element || {
-	createElement: React.createElement,
-};
