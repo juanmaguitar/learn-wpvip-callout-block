c7d8a3ebebd282ae94d74ba7f88decaacffe6d3b
JuanMa <juanma.garrido@gmail.com>
2025-04-07 09:17:04 +0100

    Update .gitignore and asset files for Learn Wpvip Callout Block
    - Added 'artifacts/' and 'test/unit/__snapshots__/' to .gitignore to prevent tracking of generated files.
    - Updated index.asset.php to reflect a new version for the block, ensuring consistency in builds.
    - Refactored index.js to streamline the code structure and improve clarity.

--- build/learn-wpvip-callout-block/index.asset.php
+++ build/learn-wpvip-callout-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '8c8e75dc417968636f8a');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '1e0aa8fbd89573051290');
